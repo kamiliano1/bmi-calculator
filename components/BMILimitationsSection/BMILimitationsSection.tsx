3f6b01d18658e05b5e546157eaeeffdf58f6ec9a
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-07-27 20:04:31 +0200

    Update BMILimitationsSection.tsx

--- components/BMILimitationsSection/BMILimitationsSection.tsx
+++ components/BMILimitationsSection/BMILimitationsSection.tsx
@@ -8,12 +8,13 @@ type BMILimitationsSectionProps = {};
 const BMILimitationsSection: React.FC<BMILimitationsSectionProps> = () => {
   return (
     <section
-      className="px-6 sm:px-10 text-center mb-20 max-w-[1440px] mx-auto sm:flex sm:flex-wrap sm:justify-center xl:justify-start xl:items-center xl:px-[clamp(2.5rem,_10vw,_8.75rem)] xl:gap-y-14"
+      className="px-6 sm:px-10 text-center mb-20 max-w-[1440px] mx-auto sm:flex sm:flex-wrap sm:justify-center xl:justify-start xl:items-center 
+      xl:px-[clamp(2.5rem,_10vw,_8.75rem)] xl:gap-y-10"
       role="complementary"
     >
-      <div className="mb-14 xl:basis-[564px] xl:text-start xl:mr-[10.25rem]">
+      <div className="mb-14 xl:basis-[564px] xl:text-start xl:mr-[10.25rem] xl:mb-20">
         <h3 className="text-XM mb-8 xl:text-L">Limitations of BMI</h3>
-        <p className="max-w-[686px]">
+        <p className="max-w-[686px] ">
           Although BMI is often a practical indicator of healthy weight, it is
           not suited for every person. Specific groups should carefully consider
           their BMI outcomes, and in certain cases, the measurement may not be
